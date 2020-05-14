1ca4e9955eab505f6905c43588ce5fea126f54c9
Ben Williams <DeanPBR@gmail.com>
2020-05-14 15:16:27 -0400

    Starting the FizzBuzz Kata

--- src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,16 @@
 package org.wecancodeit.fizzbuzz;
 
-
 public class FizzBuzz {
+
+    public String say(int numberToTranslate) {
+
+        if(numberToTranslate%3 == 0){
+            return "Fizz";
+        }
+        if(numberToTranslate == 5){
+            return "Buzz";
+        }
+        return "" + numberToTranslate;
+    }
+
 }
